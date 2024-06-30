239e3db4531551eb2029120f2b9b1cb8a4f09648
Anh Bui <anhbhpd08263@fpt.edu.vn>
2024-06-30 14:48:13 +0700

    handle upload image

--- src/modules/posts/PostAddNew.jsx
+++ src/modules/posts/PostAddNew.jsx
@@ -1,32 +1,97 @@
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import Button from '@/components/button/Button';
 import { Checkbox, Radio } from '@/components/checkbox';
 import { Dropdown } from '@/components/dropdown';
 import Field from '@/components/field/Field';
 import Input from '@/components/input/Input';
 import Label from '@/components/label';
+import { postStatus } from '@/utils/constants';
 import { useForm } from 'react-hook-form';
+import slugify from 'slugify';
 import styled from 'styled-components';
 const PostAddNewStyles = styled.div``;
 
 const PostAddNew = () => {
-  const { control, watch, setValue } = useForm({
+  const { control, watch, setValue, handleSubmit } = useForm({
     mode: 'onChange',
     defaultValues: {
-      status: '',
+      title: '',
+      slug: '',
+      status: 2,
       category: '',
     },
   });
+
+  const handleSelectImage = e => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setValue('image', file);
+  };
+  const handleUploadImage = file => {
+    const storage = getStorage();
+    const storageRef = ref(storage, 'images/' + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      'state_changed',
+      snapshot => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      },
+      error => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+          console.log('File available at', downloadURL);
+        });
+      }
+    );
+  };
   const watchStatus = watch('status');
   const watchCategory = watch('category');
-  console.log('PostAddNew ~ watchCategory', watchCategory);
+
+  const addPostHandler = async values => {
+    const cloneValues = { ...values };
+    cloneValues.slug = slugify(cloneValues.slug);
+    cloneValues.status = Number(cloneValues.status);
+    handleUploadImage(cloneValues.image);
+    console.log(cloneValues);
+  };
   return (
     <PostAddNewStyles>
       <h1 className="dashboard-heading">Add new post</h1>
-      <form>
+      <form onSubmit={handleSubmit(addPostHandler)}>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
           <Field>
             <Label>Title</Label>
-            <Input control={control} placeholder="Enter your title" name="title"></Input>
+            <Input control={control} placeholder="Enter your title" name="title" required></Input>
           </Field>
           <Field>
             <Label>Slug</Label>
@@ -35,32 +100,36 @@ const PostAddNew = () => {
         </div>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
           <Field>
+            <Label>Image</Label>
+            <input type="file" name="image" onChange={handleSelectImage} />
+          </Field>
+          <Field>
             <Label>Status</Label>
             <div className="flex items-center gap-x-5">
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === 'approved'}
+                checked={+watchStatus === postStatus.APPROVED}
                 onClick={() => setValue('status', 'approved')}
-                value="approved"
+                value={postStatus.APPROVED}
               >
                 Approved
               </Radio>
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === 'pending'}
+                checked={+watchStatus === postStatus.PENDING}
                 onClick={() => setValue('status', 'pending')}
-                value="pending"
+                value={postStatus.PENDING}
               >
                 Pending
               </Radio>
               <Radio
                 name="status"
                 control={control}
-                checked={watchStatus === 'reject'}
+                checked={+watchStatus === postStatus.REJECTED}
                 onClick={() => setValue('status', 'reject')}
-                value="reject"
+                value={postStatus.REJECTED}
               >
                 Reject
               </Radio>
